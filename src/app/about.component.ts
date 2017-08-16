9b436a56fe4e6ba9f148c45fd17b3a57ee03dca1
Maren Süwer <maren.suewer@triology.de>
2017-08-16 16:38:27 +0200

    tooltipps added

--- src/app/about.component.ts
+++ src/app/about.component.ts
@@ -8,4 +8,28 @@ import { MEMBERS } from './member';
 
 export class AboutComponent {
     members = MEMBERS;
+    membersArray = new Array() as Array<Object>;;
+    numbers;
+    constructor() {
+      var counter = (MEMBERS.length-(MEMBERS.length%15))/15;
+      if(MEMBERS.length%15!=0) counter = counter +1;
+
+      for(var i=0; i<counter; i++){
+        var helper = new Array() as Array<Object>;
+        for(var j=i; j<i+15; j++){
+          helper.push(this.members[j]);
+        }
+        this.membersArray.push(helper);
+        
+      }
+     // console.log(this.membersArray);
+    }
+
+
+
+    
+
+
 }
+
+
--- src/app/member.ts
+++ src/app/member.ts
@@ -4,7 +4,12 @@ export class Member {
     github?: string; //? = optional parameter
     xing?: string;
     twitter?: string;
+    linkedin?: string;
+    email?: string;
     stackoverflow?: string;
+    bitbucket?: string;
+    blog?: string;
+    sonstiges?: string;
 }
 /*
     {
@@ -13,104 +18,149 @@ export class Member {
         github:
         xing:
         twitter:
-        stackoverflow:
+        linkedin: 
+        email:
+        stackoverflow: 
+        bitbucket:
+        blog: 
+        sonstiges:
     },
 */
 
 export const MEMBERS: Member[] = [
     {
-        name: "cesmarvin",
-        imagesource: "https://www.gravatar.com/avatar/c0f458151985798b35017de48d9ddb8b.jpg?d=identicon",
-        github:"https://github.com/cesmarvin"
-    },
-    {
-        name:"cwolfes",
+        name:"Christoph Wolfes",
         imagesource:"https://www.gravatar.com/avatar/62a6fa374a54078b89e7038283e4719a.jpg?d=identicon",
-        github:"https://github.com/cwolfes"
+        github:"https://github.com/cwolfes",
+        xing: "https://www.xing.com/profile/Christoph_Wolfes"
     },
     {
-        name:"dhuchthausen",
+        name:"Daniel Huchthausen",
         imagesource:"https://www.gravatar.com/avatar/bb7e6df6846b928c6bbb08ff1cbd7d9c.jpg?d=identicon",
         github:"https://github.com/dhuchthausen"
     },
     {
-        name:"dstulle",
+        name:"Daniel Sturm",
         imagesource:"https://www.gravatar.com/avatar/a6fcdb445d5df0ed591fb2e5e3af40d4.jpg?d=identicon",
-        github:"https://github.com/dstulle"
+        github:"https://github.com/dstulle",
+        xing: "https://www.xing.com/profile/Daniel_Sturm6",
+        twitter: "https://twitter.com/dstulle",
+        linkedin: "https://www.linkedin.com/in/dstulle/",
+        email: "daniel.sturm@cloudogu.com",
+        stackoverflow: "https://stackoverflow.com/users/483335/dstulle",
+        bitbucket: "https://bitbucket.org/dstulle/",
+        blog: "http://blog.dstulle.de/",
+        sonstiges: "http://dstulle.de/"
     },
     {
-        name:"ischindler",
+        name:"Iwan Schindler",
         imagesource:"https://www.gravatar.com/avatar/3254f4fee43d1e9824435cc65c78a977.jpg?d=identicon",
-        github:"https://github.com/ischindler"
+        github:"https://github.com/ischindler",
+        xing: "https://www.xing.com/profile/Iwan_Schindler",
+        email: "iwan.schindler@cloudogu.com"
     },
     {
-        name:"janniemann",
+        name:"Jan Niemann",
         imagesource:"https://www.gravatar.com/avatar/35407d6c1a9568b89e31c46671281efd.jpg?d=identicon",
         github:"https://github.com/janniemann"
     },
     {
-        name:"joshavg",
-        imagesource:"https://www.gravatar.com/avatar/94f7f6d9da00a5d3bfdac698957cb083.jpg?d=identicon",
-        github:"https://github.com/joshavg"
+        name:"Johannes Schnatterer",
+        imagesource:"https://www.gravatar.com/avatar/619b9e796cddf5582c999a5f5a782a59.jpg?d=identicon",
+        github:"https://github.com/schnatterer",
+        xing:"https://www.xing.com/profile/Johannes_Schnatterer",
+        twitter: "https://twitter.com/jschnatterer",
+        email: "johannes.schnatter@cloudogu.com",
+        stackoverflow: "https://stackoverflow.com/users/1845976/",
+        blog: "http://blog.schnatterer.info/"
     },
     {
-        name:"LennartTrio",
-        imagesource:"https://www.gravatar.com/avatar/87740608aa1b3b28469ffc671d012c30.jpg?d=identicon",
-        github:"https://github.com/LennartTrio"
-    },
+        name:"Lennart Ertingshausen",
+        imagesource: "https://www.gravatar.com/avatar/8e5c6bb25c3cc0bcc699386270bd92ea.jpg?d=identicon"
+    },    
     {
-        name:"maltek709",
-        imagesource:"https://www.gravatar.com/avatar/b229a81546bf9cb79c0cbe99158199b3.jpg?d=identicon",
-        github:"https://github.com/maltek709"
-    },
-    {
-        name:"mbehlendorf",
-        imagesource:"https://www.gravatar.com/avatar/e5781ad28707dcf560fa1674d076ae59.jpg?d=identicon",
-        github:"https://github.com/mbehlendorf"
-    },
-    {
-        name:"msuewer",
+        name:"Maren Süwer",
         imagesource:"https://www.gravatar.com/avatar/20ad92404767b07a7ca0ce8eb8fa36d9.jpg?d=identicon",
-        github:"https://github.com/msuewer"
+        github:"https://github.com/msuewer",
+        email: "maren.suewer@cloudogu.com"
     },
     {
-        name:"omilke",
+        name:"Michael Behlendorf",
+        imagesource:"https://www.gravatar.com/avatar/e5781ad28707dcf560fa1674d076ae59.jpg?d=identicon",
+        github:"https://github.com/mbehlendorf",
+        email: "michael.behlendorf@cloudogu.com"
+    },
+    {   name: "Miriam Malorny",
+        imagesource: "https://www.gravatar.com/avatar/f8bcbe464694b14c28cfb544fb4ba5f6.jpg?d=identicon",
+        xing: "https://www.xing.com/profile/Miriam_Malorny",
+        email: "miriam.malorny@cloudogu.com"
+    },
+    {
+        name: "Monique Porner",
+        imagesource: "https://www.gravatar.com/avatar/e69dedf955f228f8496fd100c09c8554.jpg?d=identicon",
+        github: "https://github.com/mporner",
+        email: "monique.porner@cloudogu.com"
+    },
+    {
+        name: "Nadja Kaul",
+        imagesource: "https://www.gravatar.com/avatar/fad8e5284a8ee391102b3afdcefe3fd7.jpg?d=identicon",
+        xing: "https://www.xing.com/profile/Nadja_Kaul",
+        email: "nadja.kaul@cloudogu.com"
+    },
+    {
+        name:"Oliver Milke",
         imagesource:"https://www.gravatar.com/avatar/dbe14e77409eb6d1e991ff11844a5cc4.jpg?d=identicon",
-        github:"https://github.com/omilke"
+        github:"https://github.com/omilke",
+        twitter: "https://twitter.com/OliverMilke",
+        email: "oliver.milke@cloudogu.com",
+        stackoverflow: "https://stackoverflow.com/users/2108919/omilke",
+        blog: "http://oliver-milke.de/"
     },
     {
-        name:"pczora",
+        name:"Philipp Czora",
         imagesource:"https://www.gravatar.com/avatar/546926e6e834a6dcb81ec0cfc06bc3d5.jpg?d=identicon",
-        github:"https://github.com/pczora"
+        github:"https://github.com/pczora",
+        twitter: "https://twitter.com/_pczora",
+        stackoverflow: "https://stackoverflow.com/users/1219392/pczora"
     },
     {
-        name:"pfassnacht",
-        imagesource:"https://www.gravatar.com/avatar/b87c6de71af1b016106f8e7eb99eaccc.jpg?d=identicon",
-        github:"https://github.com/pfassnacht"
-    },
-    {
-        name:"robertauer",
+        name:"Robert Auer",
         imagesource:"https://www.gravatar.com/avatar/52ead169cd6051ecd92865c039a2ff79.jpg?d=identicon",
         github:"https://github.com/robertauer"
     },
     {
-        name:"schnatterer",
-        imagesource:"https://www.gravatar.com/avatar/619b9e796cddf5582c999a5f5a782a59.jpg?d=identicon",
-        github:"https://github.com/schnatterer"
+        name: "Rolf Hein",
+        imagesource: "https://www.gravatar.com/avatar/89c32c549ae70b7657cf8aeeb839dcce.jpg?d=identicon"
     },
     {
-        name:"schristann",
-        imagesource:"https://www.gravatar.com/avatar/6e28344b2f51f06f832d7e4b60a9e78e.jpg?d=identicon",
-        github:"https://github.com/schristann"
+        name: "Sabrina Keller",
+        imagesource: "https://www.gravatar.com/avatar/66a618c9d044a31efd795a0cfa9e136b.jpg?d=identicon",
+        xing: "https://www.xing.com/profile/Sabrina_Keller",
+        email: "sabrina.keller@cloudogu.com"
     },
     {
-        name:"sdorra",
+        name:"Sebastian Sdorra",
         imagesource:"https://www.gravatar.com/avatar/c8afb88b3858e1202d582122f94ed5f6.jpg?d=identicon",
-        github:"https://github.com/sdorra"
+        github:"https://github.com/sdorra",
+        twitter: "https://twitter.com/@ssdorra",
+        stackoverflow: "https://stackoverflow.com/users/579777/sdorra",
+        bitbucket: "https://bitbucket.org/sdorra"
     },
     {
-        name:"tgrosser",
-        imagesource:"https://www.gravatar.com/avatar/71df286b7e57c59d40b86b2d67d19b63.jpg?d=identicon",
-        github:"https://github.com/tgrosser"
+        name:"Stephan Christiann",
+        imagesource:"https://www.gravatar.com/avatar/2e6371a9e270fd8aa9e6570a88f3b14e.jpg?d=identicon"
     },
+
+    {
+        name:"Thomas Grosser",
+        imagesource:"https://www.gravatar.com/avatar/71df286b7e57c59d40b86b2d67d19b63.jpg?d=identicon"
+    },
+    {
+        name: "Thomas Paliga",
+        imagesource: "https://www.gravatar.com/avatar/b9df2a59bacf0997a01618e9c0e31f86.jpg?d=identicon"
+    },
+    {
+        name: "Tino Friedrich",
+        imagesource: "https://www.gravatar.com/avatar/e744c64db3edf0d2e84eeafda33de9eb.jpg?d=identicon"
+    }
 ];
